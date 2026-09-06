5073a45e42f2ab4644f76eb664c43f000d1c5343
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-06 20:47:13 +0200

    added booking controller

---
+++ BookMyHome/src/BookingService/BookingService.Api/Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using BookingService.FacadeLib.Commands.DTOs;
+using BookingService.FacadeLib.Commands.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookingController(ICreateBookingHandler create) : ControllerBase
+    {
+        [HttpPut(Name = "Create")]
+        public CreateBookingCommand MakeBooking()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
