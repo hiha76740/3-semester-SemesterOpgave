9b9c9fe54708f8d5825d7fcbba5bbe3330cf909d
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 18:37:22 +0200

    Updated controller with code

--- BookMyHome/src/BookingService/BookingService.Api/Controllers/BookingController.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Controllers/BookingController.cs
@@ -1,18 +1,64 @@
-﻿using BookingService.FacadeLib.Commands.DTOs;
-using BookingService.FacadeLib.Commands.Interfaces;
+﻿using BookingService.FacadeLib.Commands.Interfaces;
+using BookingService.FacadeLib.Queries.DTOs;
+using BookingService.FacadeLib.Queries.Interfaces;
+using BookMyHome.ContractsLib.Responses.BookingService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingService.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BookingController(ICreateBookingHandler create) : ControllerBase
+    public class BookingController(ICreateBookingHandler create, IBookingQueries queries) : ControllerBase
     {
-        [HttpPut(Name = "Create")]
-        public CreateBookingCommand MakeBooking()
+        [HttpPost]
+        public StatusCodeResult MakeBooking()
         {
             throw new NotImplementedException();
         }
 
+
+        [HttpGet]
+        public async Task<IReadOnlyList<BookingResponse>> GetAll()
+        {
+            var list = await queries.GetAllAsync();
+
+            var response = new List<BookingResponse>();
+
+            foreach (var item in list)
+            {
+                var bookingReponse = MapResponse(item);
+
+                response.Add(bookingReponse);
+            }
+
+            return response;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<BookingResponse> GetById(Guid id)
+        {
+            var dto = await queries.GetBookingByIdAsync(id);
+
+            if (dto != null)
+                return MapResponse(dto);
+
+            else
+                throw new NotImplementedException();
+        }
+
+        private BookingResponse MapResponse(BookingDTO DTO)
+        {
+            var output = new BookingResponse(
+                DTO.Id,
+                DTO.GuestId,
+                DTO.AccomodationId,
+                DTO.StartDate,
+                DTO.EndDate,
+                DTO.Price
+                );
+
+            return output;
+        }
     }
+
 }
