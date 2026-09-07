d3284045a9a3b05a35a095dd5b96b1af02cfff72
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 06:07:03 +0200

    Renamed AccomodationRepository to AccomodationService

--- BookMyHome/src/BookingService/BookingService.ApplicationLib/Repositories/IAccomodationRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using BookingService.DomainLib.Entities;
-
-namespace BookingService.ApplicationLib.Repositories;
-
-public interface IAccomodationRepository
-{
-    public Accomodation GetAccomodationByIdAsync(AccomodationId id);
-}
---
+++ BookMyHome/src/BookingService/BookingService.ApplicationLib/Services/IAccomodationService.cs
@@ -0,0 +1,8 @@
+﻿using BookingService.DomainLib.ValueObjects;
+
+namespace BookingService.ApplicationLib.Services;
+
+public interface IAccomodationService
+{
+    Task<bool> AccomodationExistAsync(AccomodationId id);
+}
