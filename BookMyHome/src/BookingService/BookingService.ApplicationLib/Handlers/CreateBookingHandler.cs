9521f04f5afbddc41d769783ea03e526bae06813
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 06:09:29 +0200

    Refactor CreateBookingHandler to use service checks
    Replaced repository calls with IGuestService and IAccomodationService for guest and accommodation existence checks. Handler now throws NotFoundException if either entity does not exist. Removed direct repository dependencies.

--- BookMyHome/src/BookingService/BookingService.ApplicationLib/Handlers/CreateBookingHandler.cs
+++ BookMyHome/src/BookingService/BookingService.ApplicationLib/Handlers/CreateBookingHandler.cs
@@ -1,24 +1,28 @@
 ﻿using BookingService.ApplicationLib.Repositories;
+using BookingService.ApplicationLib.Services;
 using BookingService.DomainLib.Entities;
+using BookingService.DomainLib.ValueObjects;
 using BookingService.FacadeLib.Commands.DTOs;
 using BookingService.FacadeLib.Commands.Interfaces;
 using Shared.BookMyHome.SharedKernelLib.Exceptions;
 
 namespace BookingService.ApplicationLib.Handlers;
 
-public class CreateBookingHandler(IGuestRepository guestRepo, IAccomodationRepository accomodationRepo, IBookingRepository bookingRepo) : ICreateBookingHandler
+public class CreateBookingHandler(IGuestService guestService, IAccomodationService accomodationService, IBookingRepository bookingRepo) : ICreateBookingHandler
 {
     async Task ICreateBookingHandler.Handle(CreateBookingCommand command)
     {
         var guestId = new GuestId(command.GuestId);
-
-        var guest = guestRepo.GetGuestByIdAsync(guestId) ??
-            throw new NotFoundException("Guest was not found doing booking creation");
-
         var accomodationId = new AccomodationId(command.AccomodationId);
 
-        var accomodation = accomodationRepo.GetAccomodationByIdAsync(accomodationId) ??
-            throw new NotFoundException("Accomodation was not found doing booking creation");
+        var guestExist = await guestService.GuestExistAsync(guestId);
+        var accomodationExist = await accomodationService.AccomodationExistAsync(accomodationId);
+
+        if (guestExist == false)
+            throw new NotFoundException("Guest not found doing booking creation");
+
+        if (accomodationExist == false)
+            throw new NotFoundException("Accomodation not found doing booking creation");
 
         var booking = Booking.Create(guestId, accomodationId, command.StartDate, command.EndDate, command.Price);
 
