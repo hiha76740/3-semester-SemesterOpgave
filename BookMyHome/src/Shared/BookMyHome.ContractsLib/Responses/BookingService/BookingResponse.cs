96afeff79a52c1f65e1e743b7d840712852b691c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 18:37:14 +0200

    Added BookingRepsonse
    To use between UI and API

---
+++ BookMyHome/src/Shared/BookMyHome.ContractsLib/Responses/BookingService/BookingResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace BookMyHome.ContractsLib.Responses.BookingService;
+
+public record BookingResponse(Guid Id, Guid GuestId, Guid AccomodationId, DateOnly StartDate, DateOnly EndDate, decimal Price);
