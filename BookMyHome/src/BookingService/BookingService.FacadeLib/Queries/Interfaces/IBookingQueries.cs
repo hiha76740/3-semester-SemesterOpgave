301b630b00857e1c1da1f5667360f9a6cfca6531
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 18:38:02 +0200

    Added Interface and IMPL for Booking Queries

---
+++ BookMyHome/src/BookingService/BookingService.FacadeLib/Queries/Interfaces/IBookingQueries.cs
@@ -0,0 +1,10 @@
+﻿using BookingService.FacadeLib.Queries.DTOs;
+
+namespace BookingService.FacadeLib.Queries.Interfaces;
+
+public interface IBookingQueries
+{
+    Task<IReadOnlyList<BookingDTO>> GetAllAsync();
+
+    Task<BookingDTO?> GetBookingByIdAsync(Guid Id);
+}
---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/QueryHandlers/BookingQueryHandlerIMPL.cs
@@ -0,0 +1,17 @@
+﻿using BookingService.FacadeLib.Queries.DTOs;
+using BookingService.FacadeLib.Queries.Interfaces;
+
+namespace BookingService.InfrastructureLib.QueryHandlers;
+
+public class BookingQueryHandlerIMPL : IBookingQueries
+{
+    Task<IReadOnlyList<BookingDTO>> IBookingQueries.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<BookingDTO?> IBookingQueries.GetBookingByIdAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+}
