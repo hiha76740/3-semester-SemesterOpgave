3769d347a00accff01fd1957fe76730259dd50cc
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 18:36:56 +0200

    Added BookingRepository

---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/Repositories/BookingRepository.cs
@@ -0,0 +1,27 @@
+﻿using BookingService.ApplicationLib.Repositories;
+using BookingService.DomainLib.Entities;
+
+namespace BookingService.InfrastructureLib.Repositories;
+
+public class BookingRepository : IBookingRepository
+{
+    Task IBookingRepository.AddAsync(Booking booking)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IEnumerable<Booking>> IBookingRepository.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<Booking> IBookingRepository.GetBookingByIdAsync(BookingId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IBookingRepository.SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
