503034665ff5eeb3164365c7ab4d5d8c4a559e3c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-07 16:28:12 +0200

    Added BookingPeriod unit tests

---
+++ BookMyHome/tests/BookingService/BookingService.DomainLib.Tests/BookingPeriodTests.cs
@@ -0,0 +1,48 @@
+﻿using BookingService.DomainLib.ValueObjects;
+using Shared.BookMyHome.SharedKernelLib.Exceptions;
+
+namespace BookingService.DomainLib.Tests;
+
+public class BookingPeriodTests
+{
+    private readonly static DateOnly StartDate = DateOnly.FromDateTime(DateTime.UtcNow);
+    private readonly static DateOnly EndDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(5));
+
+    private static BookingPeriod CreateBookingPeriod(
+        DateOnly? start = null,
+        DateOnly? end = null
+        )
+    {
+        return new BookingPeriod(
+            start ?? StartDate,
+            end ?? EndDate
+            );
+    }
+
+    [Fact]
+    public void Create_GivenValidBookingPeriod_SetsStartAndEndDate()
+    {
+        // Arrange
+        var expected = new BookingPeriod(StartDate, EndDate);
+
+        // Act
+        var period = CreateBookingPeriod();
+
+        // Assert
+        Assert.Equal(expected, period);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+
+    public void Create_GivenInvalidEndDate_ThrowsException(int end)
+    {
+        // Arrange
+        var endDate = StartDate.AddDays(end);
+
+        // Act & Assert
+        Assert.Throws<DomainException>(() => CreateBookingPeriod(StartDate, endDate));
+        
+    }
+}
