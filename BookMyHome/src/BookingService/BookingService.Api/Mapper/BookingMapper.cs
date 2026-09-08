8ef1af4513a575f4b2a22739b8678a57918fde11
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-08 07:16:18 +0200

    Added new method to convert from create request to create command

---
+++ BookMyHome/src/BookingService/BookingService.Api/Mapper/BookingMapper.cs
@@ -0,0 +1,38 @@
+﻿using BookingService.FacadeLib.Commands.DTOs;
+using BookingService.FacadeLib.Queries.DTOs;
+using BookMyHome.ContractsLib.Requests;
+using BookMyHome.ContractsLib.Responses.BookingService;
+
+namespace BookingService.Api.Mapper
+{
+    public static class BookingMapper
+    {
+        public static BookingResponse AsResponse(this BookingDTO DTO)
+        {
+            var output = new BookingResponse(
+                DTO.Id,
+                DTO.GuestId,
+                DTO.AccomodationId,
+                DTO.StartDate,
+                DTO.EndDate,
+                DTO.Price
+                );
+
+            return output;
+        }
+
+        public static CreateBookingCommand CreateRequestAsCommand(this CreateBookingRequest request)
+        {
+            var output = new CreateBookingCommand(
+                request.GuestId,
+                request.AccomodationId,
+                request.StartDate,
+                request.EndDate,
+                request.Price
+                );
+
+            return output;
+
+        }
+    }
+}
