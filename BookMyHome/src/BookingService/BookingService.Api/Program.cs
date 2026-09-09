f9bd4571b18a53fd2406bda459f1e4ee57985457
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-09 07:04:18 +0200

    Added calls to DI extension methods

--- BookMyHome/src/BookingService/BookingService.Api/Program.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Program.cs
@@ -1,3 +1,5 @@
+using BookingService.ApplicationLib.Extensions;
+using BookingService.InfrastructureLib.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+//builder.Services.AddHandlerDI();
+//builder.Services.AddDatabaseDI(builder.Configuration);
+//builder.Services.AddRepositoryDI();
+//builder.Services.AddQueriesDI();
+//builder.Services.AddServicesDI();
+
 
 var app = builder.Build();
 
