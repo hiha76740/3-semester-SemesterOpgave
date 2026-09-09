33e15b9591b5beba99101183852da7fa9b819b48
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-09 09:03:01 +0200

    Added placeholder Guest service class

---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/Services/GuestService.cs
@@ -0,0 +1,12 @@
+﻿using BookingService.ApplicationLib.Services;
+using BookingService.DomainLib.ValueObjects;
+
+namespace BookingService.InfrastructureLib.Services;
+
+public class GuestService : IGuestService
+{
+    Task<bool> IGuestService.GuestExistAsync(GuestId id)
+    {
+        throw new NotImplementedException();
+    }
+}
