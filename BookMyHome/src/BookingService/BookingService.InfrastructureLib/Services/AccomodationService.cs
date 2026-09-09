49bbb53c615c1938e89d10fc41c15616192674cb
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-09 09:02:47 +0200

    Added placeholder AccomodationService class

---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/Services/AccomodationService.cs
@@ -0,0 +1,12 @@
+﻿using BookingService.ApplicationLib.Services;
+using BookingService.DomainLib.ValueObjects;
+
+namespace BookingService.InfrastructureLib.Services;
+
+public class AccomodationService : IAccomodationService
+{
+    Task<bool> IAccomodationService.AccomodationExistAsync(AccomodationId id)
+    {
+        throw new NotImplementedException();
+    }
+}
