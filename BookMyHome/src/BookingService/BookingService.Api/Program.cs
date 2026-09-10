c6929422b5ffca31a337d022e82c3b9c1d6f2e13
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-10 08:02:56 +0200

    moved openapi mapping outside of dev for test

--- BookMyHome/src/BookingService/BookingService.Api/Program.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Program.cs
@@ -23,10 +23,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    
 
 }
-
+app.MapOpenApi();
 app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
