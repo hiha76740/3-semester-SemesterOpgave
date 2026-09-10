424f4a463b628afcb7a4d8f9d7e771a947e562ae
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-10 08:19:40 +0200

    Moved openapi and scalar mapping back into production

--- BookMyHome/src/BookingService/BookingService.Api/Program.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Program.cs
@@ -23,11 +23,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
-
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-app.MapOpenApi();
-app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
