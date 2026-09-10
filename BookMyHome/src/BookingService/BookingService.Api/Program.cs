63eadb870a3ad36b10b7d6364fb2e9f5811837ae
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-10 07:35:52 +0200

    uncommented AddDatabaseDI

--- BookMyHome/src/BookingService/BookingService.Api/Program.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddOpenApi();
 
 
 builder.Services.AddHandlerDI();
-//builder.Services.AddDatabaseDI(builder.Configuration);
+builder.Services.AddDatabaseDI(builder.Configuration);
 builder.Services.AddRepositoryDI();
 builder.Services.AddQueriesDI();
 builder.Services.AddServicesDI();
