2ac5f6ba4b69a0d84cac6f8eb83accfdd5f4a872
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-10 07:36:38 +0200

    Added EF configuration for Booking

---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/Persistence/EF_Configurations/BookingConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using BookingService.DomainLib.Entities;
+using BookingService.DomainLib.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingService.InfrastructureLib.Persistence.EF_Configurations;
+
+internal class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Id)
+            .HasConversion(
+            id => id.Value,
+            value => new BookingId(value));
+
+        builder.Property(b => b.GuestId)
+            .HasConversion(
+            id => id.Value,
+            value => new GuestId(value));
+
+        builder.Property(b => b.AccomodationId)
+            .HasConversion(
+            id => id.Value,
+            value => new AccomodationId(value));
+
+        builder.Property(b => b.Status)
+            .HasConversion<string>();
+
+        builder.ComplexProperty(
+            b => b.Period,
+            p => p.ToJson());
+
+
+    }
+}
