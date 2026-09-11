9549bb7e50e8bfe49d74adabf8a395c89092d1e5
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-11 09:31:01 +0200

    removed comments and added options to scalar mapping

--- BookMyHome/src/BookingService/BookingService.Api/Program.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Program.cs
@@ -4,10 +4,7 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 
@@ -24,7 +21,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "Booking API";
+    });
 }
 
 app.UseHttpsRedirection();
