80410948174f01f3b4a5b61e445415af52d7b2fd
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-11 06:23:01 +0200

    Added Happy Path test for create booking handler

--- BookMyHome/tests/BookingService/BookingService.ApplicationLib.Tests/CreateBookingTests.cs
+++ BookMyHome/tests/BookingService/BookingService.ApplicationLib.Tests/CreateBookingTests.cs
@@ -1,6 +1,58 @@
-﻿namespace BookingService.ApplicationLib.Tests;
+﻿using BookingService.ApplicationLib.Handlers;
+using BookingService.ApplicationLib.Repositories;
+using BookingService.ApplicationLib.Services;
+using BookingService.DomainLib.Entities;
+using BookingService.DomainLib.ValueObjects;
+using BookingService.FacadeLib.Commands.DTOs;
+using BookingService.FacadeLib.Commands.Interfaces;
+using Moq;
+
+namespace BookingService.ApplicationLib.Tests;
 
 public class CreateBookingTests
 {
+    [Fact]
+    public async Task Handle_GivenValidData_CallsAddAndSave()
+    {
+        // Arrange
+        var guestId = new GuestId(Guid.NewGuid());
+        var accomodationId = new AccomodationId(Guid.NewGuid());
+        var startDate = DateOnly.FromDateTime(DateTime.UtcNow);
+        var endDate = startDate.AddDays(5);
+        var price = 5000m;
 
+
+        var mockGuestService = new Mock<IGuestService>();
+        var mockAccomodationService = new Mock<IAccomodationService>();
+        var mockBookingRepo = new Mock<IBookingRepository>();
+
+        mockGuestService
+            .Setup(s => s.GuestExistAsync(guestId))
+            .ReturnsAsync(true);
+
+        mockAccomodationService
+            .Setup(s => s.AccomodationExistAsync(accomodationId))
+            .ReturnsAsync(true);
+
+        mockBookingRepo
+            .Setup(r => r.GetAllAsync())
+            .ReturnsAsync(new List<Booking>());
+
+
+        var command = new CreateBookingCommand(
+            guestId.Value,
+            accomodationId.Value,
+            startDate,
+            endDate,
+            price);
+
+        var handler = new CreateBookingHandler(mockGuestService.Object, mockAccomodationService.Object, mockBookingRepo.Object) as ICreateBookingHandler;
+
+        // Act
+        await handler.Handle(command);
+
+        // Assert
+        mockBookingRepo.Verify(r => r.AddAsync(It.IsAny<Booking>()),Times.Once);
+        mockBookingRepo.Verify(r => r.SaveAsync(), Times.Once);
+    }
 }
