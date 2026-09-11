201027bd9f0c58fd540937ba979bd318e46edf52
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-11 07:46:42 +0200

    added accomodation class in accomodation service

---
+++ BookMyHome/src/AccomodationService/AccomodationService.DomainLib/Entities/Accomodation.cs
@@ -0,0 +1,31 @@
+﻿using Shared.BookMyHome.SharedKernelLib.Exceptions;
+
+namespace AccomodationService.DomainLib.Entities;
+
+public class Accomodation
+{
+    public AccomodationId Id { get; init; }
+    public string Title { get; init; }
+
+
+
+    public static Accomodation Create(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new DomainException("Title can not be empty");
+
+        var accomodation = new Accomodation(title);
+
+        return accomodation;
+    }
+
+
+    private Accomodation(string title)
+    {
+        Id = new AccomodationId(Guid.NewGuid());
+        Title = title;
+    }
+
+    // EF constructor
+    private Accomodation() { }
+}
