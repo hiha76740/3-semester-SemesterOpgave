4500ecb6d830291bd0e24dd6ec7954fac8de8412
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-12 07:49:01 +0200

    Added documentation for all booking endpoints

--- BookMyHome/src/BookingService/BookingService.Api/Controllers/BookingsController.cs
+++ BookMyHome/src/BookingService/BookingService.Api/Controllers/BookingsController.cs
@@ -4,6 +4,7 @@ using BookingService.FacadeLib.Queries.Interfaces;
 using BookMyHome.ContractsLib.Requests;
 using BookMyHome.ContractsLib.Responses.BookingService;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 namespace BookingService.Api.Controllers
 {
@@ -12,41 +13,82 @@ namespace BookingService.Api.Controllers
     public class BookingsController(ICreateBookingHandler create, IBookingQueries queries) : ControllerBase
     {
         [HttpPost]
-        public async Task<StatusCodeResult> MakeBooking(CreateBookingRequest request)
+        [EndpointSummary("This endpoint will create a booking")]
+        [EndpointDescription("Creates a booking when all required info is given")]
+        [ProducesResponseType(StatusCodes.Status200OK, Description = "Booking was created succesfully")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error doing creation of booking")]
+        public async Task<ActionResult> MakeBooking(
+            [Description("Information requried to create a booking")] CreateBookingRequest request)
         {
-            await create.Handle(request.CreateRequestAsCommand());
 
-            return Ok();
+            try
+            {
+                await create.Handle(request.CreateRequestAsCommand());
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
         }
 
 
         [HttpGet]
+        [EndpointSummary("This endpoint will get all bookings")]
+        [EndpointDescription("Gets all bookings or returns not found if no bookings")]
+        [ProducesResponseType<IReadOnlyList<BookingResponse>>(StatusCodes.Status200OK, "application/json", Description = "Returns list of all bookings")]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Description = "No bookings was found")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error while receiving all bookings")]
         public async Task<ActionResult<IReadOnlyList<BookingResponse>>> GetAll()
         {
-            var list = await queries.GetAllAsync();
-
-            if (list.Count == 0)
-                return NotFound("No bookings was found");
-
-            var response = new List<BookingResponse>();
-
-            foreach (var item in list)
+            try
             {
-                response.Add(item.AsResponse());
-            }
+                var list = await queries.GetAllAsync();
 
-            return Ok(response);
+                if (list.Count == 0)
+                    return NotFound("No bookings was found");
+
+                var response = new List<BookingResponse>();
+
+                foreach (var item in list)
+                {
+                    response.Add(item.AsResponse());
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
         }
 
         [HttpGet("{id:guid}")]
-        public async Task<ActionResult<BookingResponse>> GetById(Guid id)
+        [EndpointSummary("This endpoint will get a specific booking")]
+        [EndpointDescription("Gets the booking for the entered id or returns not found if no booking")]
+        [ProducesResponseType<BookingResponse>(StatusCodes.Status200OK, "application/json", Description = "Returns the requested booking")]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Description = "Requested booking not found")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error while receiving requsted booking")]
+        public async Task<ActionResult<BookingResponse>> GetById(
+            [Description("Id of the booking you want to find")] Guid id)
         {
-            var dto = await queries.GetBookingByIdAsync(id);
+            try
+            {
+                var dto = await queries.GetBookingByIdAsync(id);
 
-            if (dto == null)
-                return NotFound("No booking was found");
+                if (dto == null)
+                    return NotFound("No booking was found");
 
-            return Ok(dto.AsResponse());
+                return Ok(dto.AsResponse());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
         }
     }
 
