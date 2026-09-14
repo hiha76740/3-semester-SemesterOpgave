c0d26e9fd3480a450e2f9220819c29e15c242df6
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-14 19:43:59 +0200

    Added AccomodationTests file with happy path test

---
+++ BookMyHome/tests/AccomodationService/AccomodationService.DomainLib.Tests/AccomodationTests.cs
@@ -0,0 +1,30 @@
+﻿using AccomodationService.DomainLib.Entities;
+using AccomodationService.DomainLib.Enums;
+
+namespace AccomodationService.DomainLib.Tests;
+
+public class AccomodationTests
+{
+    private readonly static string Title = "Test title";
+
+    private static Accomodation CreateAccomodation(
+        string? title = null
+        )
+    {
+        return Accomodation.Create(
+            title ?? Title);
+    }
+
+    [Fact]
+    public void Create_GivenValidAccomodation_SetsStatusToActive()
+    {
+        // Arrange
+        var expected = AccomodationStatus.Active;
+
+        // Act
+        var accomodation = CreateAccomodation();
+
+        // Assert
+        Assert.Equal(expected, accomodation.Status);
+    }
+}
