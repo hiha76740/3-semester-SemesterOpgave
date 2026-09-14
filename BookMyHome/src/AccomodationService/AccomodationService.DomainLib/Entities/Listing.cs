27862d4d89d543bad40157266cc2e586e8953a90
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-14 12:12:51 +0200

    Added Listing class with methods

---
+++ BookMyHome/src/AccomodationService/AccomodationService.DomainLib/Entities/Listing.cs
@@ -0,0 +1,60 @@
+﻿using AccomodationService.DomainLib.Enums;
+using Shared.BookMyHome.SharedKernelLib.Exceptions;
+
+namespace AccomodationService.DomainLib.Entities;
+
+public class Listing
+{
+    public ListingId Id { get; init; }
+
+    public string ListingName { get; init; }
+
+    public decimal DailyPrice { get; private set; }
+
+    public string HouseRules { get; private set; }
+
+    public AccomodationType Type { get; init; }
+
+
+
+
+    internal void UpdateDailyPrice(decimal newPrice)
+    {
+        if (newPrice == DailyPrice)
+            throw new DomainException("Old and new price can not be the same");
+
+        if (newPrice <= 0)
+            throw new DomainException("new price can not be 0 or negative");
+
+        DailyPrice = newPrice;
+    }
+
+    internal void UpdateHouseRules(string newHouseRules)
+    {
+        if (string.IsNullOrWhiteSpace(newHouseRules))
+            throw new DomainException("new house rules can not be empty");
+
+        HouseRules = newHouseRules;
+    }
+
+    internal Listing(string listingName, decimal dailyPrice, string houseRules, AccomodationType type)
+    {
+        if (string.IsNullOrWhiteSpace(listingName))
+            throw new DomainException("listing name can not be empty");
+
+        if (dailyPrice <= 0)
+            throw new DomainException("Daily price can not be 0 or negative");
+
+        if (string.IsNullOrWhiteSpace(houseRules))
+            throw new DomainException("House rules can not be empty");
+
+        Id = new ListingId(Guid.NewGuid());
+        ListingName = listingName;
+        DailyPrice = dailyPrice;
+        HouseRules = houseRules;
+        Type = type;
+    }
+
+
+    private Listing() { }
+}
