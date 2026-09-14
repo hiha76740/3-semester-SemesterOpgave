31132853b53b23f3c853ca17b07f7cf3c2c51e33
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-14 12:13:05 +0200

    updated Accomodation class

--- BookMyHome/src/AccomodationService/AccomodationService.DomainLib/Entities/Accomodation.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.DomainLib/Entities/Accomodation.cs
@@ -1,4 +1,5 @@
-﻿using Shared.BookMyHome.SharedKernelLib.Exceptions;
+﻿using AccomodationService.DomainLib.Enums;
+using Shared.BookMyHome.SharedKernelLib.Exceptions;
 
 namespace AccomodationService.DomainLib.Entities;
 
@@ -7,6 +8,53 @@ public class Accomodation
     public AccomodationId Id { get; init; }
     public string Title { get; init; }
 
+    private readonly List<Listing> _listings = [];
+    public IReadOnlyList<Listing> listings => _listings.AsReadOnly();
+
+
+    public void CreateListing(string listingName, decimal dailyPrice, string houseRules, AccomodationType type)
+    {
+        var listing = new Listing(listingName, dailyPrice, houseRules, type);
+
+        _listings.Add(listing);
+    }
+
+    public void RemoveListing(Guid id)
+    {
+        var listingId = new ListingId(id);
+
+        var listing = GetListing(listingId);
+
+        _listings.Remove(listing);
+    }
+
+    public void UpdateListingDailyPrice(Guid id, decimal newPrice)
+    {
+        var listingId = new ListingId(id);
+
+        var listing = GetListing(listingId);
+
+        listing.UpdateDailyPrice(newPrice);
+    }
+
+    public void UpdateHouseRules(Guid id, string newHouseRules)
+    {
+        var listingId = new ListingId(id);
+
+        var listing = GetListing(listingId);
+
+        listing.UpdateHouseRules(newHouseRules);
+    }
+
+    private Listing GetListing(ListingId id)
+    {
+        var listing = _listings.FirstOrDefault(l => l.Id == id);
+
+        if (listing == null)
+            throw new DomainException("No losting was found");
+
+        return listing;
+    }
 
 
     public static Accomodation Create(string title)
@@ -18,7 +66,6 @@ public class Accomodation
 
         return accomodation;
     }
-
 
     private Accomodation(string title)
     {
