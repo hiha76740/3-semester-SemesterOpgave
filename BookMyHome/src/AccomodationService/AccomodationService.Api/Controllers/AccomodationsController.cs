83331bdf5b20620972574a026298ae252111fbd6
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 09:33:48 +0200

    Added GetById and GetAll endpoints

--- BookMyHome/src/AccomodationService/AccomodationService.Api/Controllers/AccomodationsController.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Controllers/AccomodationsController.cs
@@ -1,13 +1,16 @@
 ﻿using AccomodationService.Api.Mapper;
 using AccomodationService.FacadeLib.Commands.Interfaces;
-using BookMyHome.ContractsLib.Requests;
+using AccomodationService.FacadeLib.Queries.Interfaces;
+using BookMyHome.ContractsLib.Requests.Accomodations;
+using BookMyHome.ContractsLib.Responses.Accomodations;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 namespace AccomodationService.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
-    public class AccomodationsController(ICreateAccomodationHandler create) : ControllerBase
+    public class AccomodationsController(ICreateAccomodationHandler create, IAccomodationQueries queries) : ControllerBase
     {
         [HttpPost]
         [EndpointSummary("This endpoint will create a accomodation")]
@@ -21,6 +24,62 @@ namespace AccomodationService.Api.Controllers
                 await create.Handle(request.CreateRequestAsCommand());
 
                 return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [EndpointSummary("This endpoint will get all accomodations")]
+        [EndpointDescription("Gets all accomodations or returns not found if no accomodations was found")]
+        [ProducesResponseType<IReadOnlyList<AccomodationResponse>>(StatusCodes.Status200OK, "application/json", Description = "Returns list of all bookings")]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Description = "No accomodations was found")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error while receiving all accomodations")]
+        public async Task<ActionResult<IReadOnlyList<AccomodationResponse>>> GetAll()
+        {
+            try
+            {
+                var list = await queries.GetAllAccomodationsAsync();
+
+                if (list.Count == 0)
+                    return NotFound("No accomodations found");
+
+                var response = new List<AccomodationResponse>();
+
+                foreach (var item in list)
+                {
+                    response.Add(item.AsResponse());
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{id:guid}")]
+        [EndpointSummary("This endpoint will get a specific accomodation")]
+        [EndpointDescription("Gets the accomodation for the entered id or returns not found if no accomodation was found")]
+        [ProducesResponseType<AccomodationResponse>(StatusCodes.Status200OK, "application/json", Description = "Returns the requested accomodation")]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Description = "Requested accomodation not found")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error while receiving requsted accomodation")]
+        public async Task<ActionResult<AccomodationResponse>> GetById(
+            [Description("Id of the accomodation you want to find")] Guid id)
+        {
+            try
+            {
+                var dto = await queries.GetAccomodationByIdAsync(id);
+
+                if (dto == null)
+                    return NotFound("No accomodation was found");
+
+                return Ok(dto.AsResponse());
             }
             catch (Exception ex)
             {
