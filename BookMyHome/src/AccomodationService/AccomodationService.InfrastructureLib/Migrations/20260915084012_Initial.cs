f16490e3b20e3b544ce4ed6fd9f62e32b5587775
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 10:41:58 +0200

    Added Migration

---
+++ BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/Migrations/20260915084012_Initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccomodationService.InfrastructureLib.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accomodations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accomodations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Listing",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ListingName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DailyPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    HouseRules = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AccomodationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Listing", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Listing_Accomodations_AccomodationId",
+                        column: x => x.AccomodationId,
+                        principalTable: "Accomodations",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Listing_AccomodationId",
+                table: "Listing",
+                column: "AccomodationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Listing");
+
+            migrationBuilder.DropTable(
+                name: "Accomodations");
+        }
+    }
+}
