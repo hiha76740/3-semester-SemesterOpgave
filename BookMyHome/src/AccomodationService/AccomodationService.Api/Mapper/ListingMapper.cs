55d7f4a5da3bb9d300fe4095e346edc7ffd3ef75
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 14:32:57 +0200

    added listing mapper extension method

---
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/ListingMapper.cs
@@ -0,0 +1,21 @@
+﻿using AccomodationService.FacadeLib.Queries.DTOs;
+using BookMyHome.ContractsLib.Responses.Accomodations;
+
+namespace AccomodationService.Api.Mapper;
+
+public static class ListingMapper
+{
+    public static ListingResponse AsReponse(this ListingDto dto)
+    {
+        var output = new ListingResponse(
+            dto.ListingId,
+            dto.AccomodationId,
+            dto.ListingName,
+            dto.DailyPrice,
+            dto.HouseRules,
+            dto.AccomodationType
+            );
+
+        return output;
+    }
+}
