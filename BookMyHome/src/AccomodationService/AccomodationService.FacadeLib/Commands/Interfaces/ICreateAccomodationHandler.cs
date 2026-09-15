f66f708730f278bdafcd8f0462a8cd907d998b47
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 06:12:20 +0200

    added interface for create Accomodation Handler

---
+++ BookMyHome/src/AccomodationService/AccomodationService.FacadeLib/Commands/Interfaces/ICreateAccomodationHandler.cs
@@ -0,0 +1,8 @@
+﻿using AccomodationService.FacadeLib.Commands.DTOs;
+
+namespace AccomodationService.FacadeLib.Commands.Interfaces;
+
+public interface ICreateAccomodationHandler
+{
+    Task Handle(CreateAccomodationCommand command);
+}
