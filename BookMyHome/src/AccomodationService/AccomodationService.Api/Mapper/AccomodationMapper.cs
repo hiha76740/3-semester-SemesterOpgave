353d75608b60e461f65f504560542732d0db00d8
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 10:19:28 +0200

    Added Id to AsResponse method

--- BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/AccomodationMapper.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/AccomodationMapper.cs
@@ -10,6 +10,7 @@ namespace AccomodationService.Api.Mapper
         public static AccomodationResponse AsResponse(this AccomodationDto Dto)
         {
             var output = new AccomodationResponse(
+                Dto.Id,
                 Dto.HostId,
                 Dto.Title
                 );
