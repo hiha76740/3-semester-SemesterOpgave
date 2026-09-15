6b5e390e1efa330b5b9a2490a21f04cc4fef6e1a
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 06:39:27 +0200

    Added test for Create Accomodation Handler

---
+++ BookMyHome/tests/AccomodationService/AccomodationService.ApplicationLib.Tests/CreateAccomodationTests.cs
@@ -0,0 +1,36 @@
+﻿using AccomodationService.ApplicationLib.Handlers;
+using AccomodationService.ApplicationLib.Repositories;
+using AccomodationService.DomainLib.Entities;
+using AccomodationService.DomainLib.ValueObjects;
+using AccomodationService.FacadeLib.Commands.DTOs;
+using AccomodationService.FacadeLib.Commands.Interfaces;
+using Moq;
+
+namespace AccomodationService.ApplicationLib.Tests;
+
+public class CreateAccomodationTests
+{
+    [Fact]
+    public async Task Handle_GivenValidData_CallsAddAndSave()
+    {
+        // Arrange
+        var hostId = new HostId(Guid.NewGuid());
+        var title = "My new house";
+
+        var mockAccomodationRepo = new Mock<IAccomodationRepository>();
+
+        var command = new CreateAccomodationCommand(
+            hostId.Value,
+            title
+            );
+
+        var handler = new CreateAccomodationHandler(mockAccomodationRepo.Object) as ICreateAccomodationHandler;
+
+        // Act
+        await handler.Handle(command);
+
+        // Assert
+        mockAccomodationRepo.Verify(r => r.CreateAsync(It.IsAny<Accomodation>()), Times.Once);
+        mockAccomodationRepo.Verify(r => r.SaveAsync(),Times.Once);
+    }
+}
