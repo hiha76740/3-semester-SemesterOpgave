8b17600afa77faf3378566ea378ba524f08d86a5
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 06:39:36 +0200

    Added hostId to test

--- BookMyHome/tests/AccomodationService/AccomodationService.DomainLib.Tests/AccomodationTests.cs
+++ BookMyHome/tests/AccomodationService/AccomodationService.DomainLib.Tests/AccomodationTests.cs
@@ -1,17 +1,21 @@
 ﻿using AccomodationService.DomainLib.Entities;
 using AccomodationService.DomainLib.Enums;
+using AccomodationService.DomainLib.ValueObjects;
 
 namespace AccomodationService.DomainLib.Tests;
 
 public class AccomodationTests
 {
+    private readonly static HostId HostId = new(Guid.Parse("4504e34a-67a5-4cba-b029-8eb0b453b80d"));
     private readonly static string Title = "Test title";
 
     private static Accomodation CreateAccomodation(
+        HostId? hostId = null,
         string? title = null
         )
     {
         return Accomodation.Create(
+            hostId ?? HostId,
             title ?? Title);
     }
 
