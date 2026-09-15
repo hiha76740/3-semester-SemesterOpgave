e82055757e8dd61879a56efa5baccbbfdf792651
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 10:20:11 +0200

    Added Accomodation Configuration IMPL

--- BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/Persistence/EF_Configurations/AccomodationConfiguration.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/Persistence/EF_Configurations/AccomodationConfiguration.cs
@@ -1,5 +1,27 @@
-﻿namespace AccomodationService.InfrastructureLib.Persistence.EF_Configurations;
+﻿using AccomodationService.DomainLib.Entities;
+using AccomodationService.DomainLib.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-internal class AccomodationConfiguration
+namespace AccomodationService.InfrastructureLib.Persistence.EF_Configurations;
+
+internal class AccomodationConfiguration : IEntityTypeConfiguration<Accomodation>
 {
+    public void Configure(EntityTypeBuilder<Accomodation> builder)
+    {
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Id)
+            .HasConversion(
+            id => id.Value,
+            value => new AccomodationId(value));
+
+        builder.Property(a => a.HostId)
+            .HasConversion(
+            id => id.Value,
+            value => new HostId(value));
+
+        builder.Property(a => a.Status)
+            .HasConversion<string>();
+    }
 }
