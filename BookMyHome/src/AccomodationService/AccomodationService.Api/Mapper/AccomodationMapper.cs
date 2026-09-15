b33039e72989960a3a1e4cbc52f67656ebedd8ef
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 07:06:13 +0200

    Added request to command mapper extension method

---
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/AccomodationMapper.cs
@@ -0,0 +1,18 @@
+﻿using AccomodationService.FacadeLib.Commands.DTOs;
+using BookMyHome.ContractsLib.Requests;
+
+namespace AccomodationService.Api.Mapper
+{
+    public static class AccomodationMapper
+    {
+        public static CreateAccomodationCommand CreateRequestAsCommand(this CreateAccomodationRequest request)
+        {
+            var output = new CreateAccomodationCommand(
+                request.HostId,
+                request.Title
+                );
+
+            return output;
+        }
+    }
+}
