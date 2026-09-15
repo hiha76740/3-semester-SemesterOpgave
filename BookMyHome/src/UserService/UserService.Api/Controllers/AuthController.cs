a6a305151e2fcd8ed1c4e4e71d6a97e5538099a6
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 18:25:50 +0200

    Added AuthController

---
+++ BookMyHome/src/UserService/UserService.Api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using BookMyHome.ContractsLib.Requests.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public Task<ActionResult> Register(RegisterUserRequest request)
+        {
+            
+        }
+
+    }
+}
