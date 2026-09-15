26f0f1367025276e1c65ee262a576607bebd664d
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 14:34:49 +0200

    added listing response dto

---
+++ BookMyHome/src/Shared/BookMyHome.ContractsLib/Responses/Accomodations/ListingResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace BookMyHome.ContractsLib.Responses.Accomodations;
+
+public record ListingResponse(Guid ListingId,Guid AccomodationId, string ListingName, decimal DailyPrice, string HouseRules, string AccomodationType);
