e6a8c922c2853b5bfdf25803d8bfea14d6dd4218
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 07:06:00 +0200

    added Accomodation Controller with Post endpoint

---
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Controllers/AccomodationsController.cs
@@ -0,0 +1,32 @@
+﻿using AccomodationService.Api.Mapper;
+using AccomodationService.FacadeLib.Commands.Interfaces;
+using BookMyHome.ContractsLib.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccomodationService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccomodationsController(ICreateAccomodationHandler create) : ControllerBase
+    {
+        [HttpPost]
+        [EndpointSummary("This endpoint will create a accomodation")]
+        [EndpointDescription("Creates a accomodation when all required info is given")]
+        [ProducesResponseType(StatusCodes.Status200OK, Description = "Accomodation was created succesfully")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Description = "Error doing creation of accomodation")]
+        public async Task<ActionResult> CreateAccomodation(CreateAccomodationRequest request)
+        {
+            try
+            {
+                await create.Handle(request.CreateRequestAsCommand());
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+    }
+}
