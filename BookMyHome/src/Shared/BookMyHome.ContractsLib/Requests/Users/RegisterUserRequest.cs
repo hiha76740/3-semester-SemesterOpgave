e4b1a01b28d19425221242b0bcab1574c4bdc27e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 18:25:10 +0200

    Added Register User request dto

---
+++ BookMyHome/src/Shared/BookMyHome.ContractsLib/Requests/Users/RegisterUserRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMyHome.ContractsLib.Requests.Users;
+
+public record RegisterUserRequest(
+    string FirstName,
+    string LastName,
+    DateOnly Birthdate,
+    string Street,
+    string PostalCode,
+    string City,
+    string PhoneNumber,
+    string Email,
+    string Password
+    );
