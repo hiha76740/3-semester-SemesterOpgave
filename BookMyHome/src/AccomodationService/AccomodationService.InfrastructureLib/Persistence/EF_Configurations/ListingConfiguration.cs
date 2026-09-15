fc61640f8d5073f1c30efbe99876e63127d98d4f
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 10:42:34 +0200

    Added Listing Configuration

---
+++ BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/Persistence/EF_Configurations/ListingConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AccomodationService.DomainLib.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccomodationService.InfrastructureLib.Persistence.EF_Configurations;
+
+internal class ListingConfiguration : IEntityTypeConfiguration<Listing>
+{
+    public void Configure(EntityTypeBuilder<Listing> builder)
+    {
+        builder.HasKey(l => l.Id);
+
+        builder.Property(l => l.Id)
+            .HasConversion(
+            id => id.Value,
+            value => new ListingId(value));
+
+        builder.Property(l => l.Type)
+            .HasConversion<string>();
+    }
+}
