3986c30328cf57616af87a55b7b55290c4f08e8a
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 07:07:55 +0200

    added Db context

---
+++ BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/Persistence/AccomodationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AccomodationService.DomainLib.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccomodationService.InfrastructureLib.Persistence;
+
+public class AccomodationDbContext : DbContext
+{
+    public DbSet<Accomodation> Accomodations { get; set; }
+
+
+}
