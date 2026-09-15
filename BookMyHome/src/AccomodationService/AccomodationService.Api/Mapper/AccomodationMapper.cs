582e367a7cf9da65a797123ce591f9b0112c19d8
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 09:34:14 +0200

    Added extension method to map to response from dto

--- BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/AccomodationMapper.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.Api/Mapper/AccomodationMapper.cs
@@ -1,10 +1,23 @@
 ﻿using AccomodationService.FacadeLib.Commands.DTOs;
-using BookMyHome.ContractsLib.Requests;
+using AccomodationService.FacadeLib.Queries.DTOs;
+using BookMyHome.ContractsLib.Requests.Accomodations;
+using BookMyHome.ContractsLib.Responses.Accomodations;
 
 namespace AccomodationService.Api.Mapper
 {
     public static class AccomodationMapper
     {
+        public static AccomodationResponse AsResponse(this AccomodationDto Dto)
+        {
+            var output = new AccomodationResponse(
+                Dto.HostId,
+                Dto.Title
+                );
+
+            return output;
+        }
+
+
         public static CreateAccomodationCommand CreateRequestAsCommand(this CreateAccomodationRequest request)
         {
             var output = new CreateAccomodationCommand(
