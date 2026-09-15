49e3ca1967ab501e1e3d3df0fbebb10f497ca219
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 07:07:20 +0200

    added HandlerDI extension method

---
+++ BookMyHome/src/AccomodationService/AccomodationService.ApplicationLib/Extensions/HandlerDI.cs
@@ -0,0 +1,15 @@
+﻿using AccomodationService.ApplicationLib.Handlers;
+using AccomodationService.FacadeLib.Commands.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccomodationService.ApplicationLib.Extensions;
+
+public static class HandlerDI
+{
+    public static IServiceCollection AddHandlerDI(this IServiceCollection services)
+    {
+        services.AddScoped<ICreateAccomodationHandler, CreateAccomodationHandler>();
+
+        return services;
+    }
+}
