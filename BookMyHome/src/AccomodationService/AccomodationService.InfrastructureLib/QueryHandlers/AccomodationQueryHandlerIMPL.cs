92617e97e52967ad98a4fd5aaf82dfed7a2ddb89
Henrik Ivang Hansen <hiha76740@edu.ucl.dk>
2026-09-15 08:09:52 +0200

    Added  IMPL for Accomodation Query handler

---
+++ BookMyHome/src/AccomodationService/AccomodationService.InfrastructureLib/QueryHandlers/AccomodationQueryHandlerIMPL.cs
@@ -0,0 +1,35 @@
+﻿using AccomodationService.DomainLib.Entities;
+using AccomodationService.FacadeLib.Queries.DTOs;
+using AccomodationService.FacadeLib.Queries.Interfaces;
+using AccomodationService.InfrastructureLib.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccomodationService.InfrastructureLib.QueryHandlers;
+
+public class AccomodationQueryHandlerIMPL(AccomodationDbContext db) : IAccomodationQueries
+{
+    async Task<AccomodationDto?> IAccomodationQueries.GetAccomodationByIdAsync(Guid id)
+    {
+        var accomodationId = new AccomodationId(id);
+
+        return await db.Accomodations
+            .AsNoTracking()
+            .Where(a => a.Id == accomodationId)
+            .Select(a => new AccomodationDto(
+                a.Id.Value,
+                a.Title
+                ))
+            .FirstOrDefaultAsync();
+    }
+
+    async Task<IReadOnlyList<AccomodationDto>> IAccomodationQueries.GetAllAccomodationsAsync()
+    {
+        return await db.Accomodations
+            .AsNoTracking()
+            .Select(a => new AccomodationDto(
+            a.Id.Value,
+            a.Title
+            ))
+            .ToListAsync();
+    }
+}
