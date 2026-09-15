99191a9c925171cc044edb8bf1895e96c9bb7735
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-15 06:11:48 +0200

    added createAccomodationHandler

---
+++ BookMyHome/src/AccomodationService/AccomodationService.ApplicationLib/Handlers/CreateAccomodationHandler.cs
@@ -0,0 +1,12 @@
+﻿using AccomodationService.FacadeLib.Commands.DTOs;
+using AccomodationService.FacadeLib.Commands.Interfaces;
+
+namespace AccomodationService.ApplicationLib.Handlers;
+
+public class CreateAccomodationHandler : ICreateAccomodationHandler
+{
+    Task ICreateAccomodationHandler.Handle(CreateAccomodationCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
