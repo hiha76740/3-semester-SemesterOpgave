bfc82a2324e265477cd3e1384f8b2c7a3593c85d
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:23:28 +0200

    Added Interface for User Repository

---
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using UserService.DomainLib.Entities;
+
+namespace UserService.ApplicationLib.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetUserByUsernameAsync(string username);
+
+    Task<bool> UsernameExsistsAsync(string username);
+
+    Task RegisterUserAsync(User user);
+
+    Task SaveAsync();
+}
