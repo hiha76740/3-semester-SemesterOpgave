bb3197ba4bae241849b9027a6d242cb7e7a5fce0
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:24:36 +0200

    Added Interface for LoginHandler

---
+++ BookMyHome/src/UserService/UserService.FacadeLib/Commands/Interfaces/ILoginHandler.cs
@@ -0,0 +1,8 @@
+﻿using UserService.FacadeLib.Commands.DTOs;
+
+namespace UserService.FacadeLib.Commands.Interfaces;
+
+public interface ILoginHandler
+{
+    Task HandleAsync(LoginCommand command);
+}
