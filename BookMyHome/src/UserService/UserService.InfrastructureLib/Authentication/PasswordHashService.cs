5a7ab0bf1a92d4bd90940ca187251e9984657ffb
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:25:15 +0200

    Added PasswordHash Service IMPL

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Authentication/PasswordHashService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserService.ApplicationLib.Authentication;
+
+namespace UserService.InfrastructureLib.Authentication;
+
+internal class PasswordHashService : IPasswordHashService
+{
+    private readonly PasswordHasher<object> _passwordHasher = new();
+    private readonly object _passwordHasherUser = new();
+
+    string IPasswordHashService.Hash(string password)
+    {
+        var hashedPassword = _passwordHasher.HashPassword(_passwordHasherUser, password);
+
+        return hashedPassword;
+    }
+
+    bool IPasswordHashService.Verify(string password, string passwordHash)
+    {
+        throw new NotImplementedException();
+    }
+}
