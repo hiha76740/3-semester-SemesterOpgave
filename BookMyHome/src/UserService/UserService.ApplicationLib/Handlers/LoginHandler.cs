da08c36c05270137656c5d8dcb476cca2af9cb10
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:22:55 +0200

    Added Login Handler IMPL

---
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Handlers/LoginHandler.cs
@@ -0,0 +1,24 @@
+﻿using Shared.BookMyHome.SharedKernelLib.Exceptions;
+using UserService.ApplicationLib.Repositories;
+using UserService.FacadeLib.Commands.DTOs;
+using UserService.FacadeLib.Commands.Interfaces;
+
+namespace UserService.ApplicationLib.Handlers
+{
+    internal class LoginHandler(IUserRepository userRepo) : ILoginHandler
+    {
+        async Task ILoginHandler.HandleAsync(LoginCommand command)
+        {
+            var user = await userRepo.GetUserByUsernameAsync(command.Username);
+
+            if (user == null)
+                throw new NotFoundException("User or password was incorrect");
+
+            
+
+            
+
+
+        }
+    }
+}
