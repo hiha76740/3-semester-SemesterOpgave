b77816f978ecd4d87e192a647fca7bccd04e981c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:24:45 +0200

    Added Interface for Register User Handler

---
+++ BookMyHome/src/UserService/UserService.FacadeLib/Commands/Interfaces/IRegisterUserHandler.cs
@@ -0,0 +1,8 @@
+﻿using UserService.FacadeLib.Commands.DTOs;
+
+namespace UserService.FacadeLib.Commands.Interfaces;
+
+public interface IRegisterUserHandler
+{
+    Task Handle(RegisterUserCommand command);
+}
