d676f9f1a6c3512fcb3c04ea08d7d55d653fe316
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:22:35 +0200

    Added interface for Password hash service

---
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Authentication/IPasswordHashService.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.ApplicationLib.Authentication;
+
+public interface IPasswordHashService
+{
+    string Hash(string password);
+
+    bool Verify(string password,string passwordHash);
+}
