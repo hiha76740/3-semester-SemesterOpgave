26a9ccd0add7e2e7e5b078084d1c156d67d846ea
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:22:09 +0200

    Added user mapper extension method

---
+++ BookMyHome/src/UserService/UserService.Api/Mapper/UserMapper.cs
@@ -0,0 +1,26 @@
+﻿using BookMyHome.ContractsLib.Requests.Users;
+using Microsoft.AspNetCore.Identity.Data;
+using UserService.FacadeLib.Commands.DTOs;
+
+namespace UserService.Api.Mapper;
+
+public static class UserMapper
+{
+    public static RegisterUserCommand AsCommand(this RegisterUserRequest request)
+    {
+        var output = new RegisterUserCommand(
+            request.FirstName,
+            request.LastName,
+            request.Birthdate,
+            request.Street,
+            request.PostalCode,
+            request.City,
+            request.PhoneNumber,
+            request.Email,
+            request.Password,
+            request.Role
+            );
+
+        return output;
+    }
+}
