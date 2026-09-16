39f48459002ca77ff24f34568e43b3e07ebbe39b
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:24:25 +0200

    Added Register User command

---
+++ BookMyHome/src/UserService/UserService.FacadeLib/Commands/DTOs/RegisterUserCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace UserService.FacadeLib.Commands.DTOs;
+
+public record RegisterUserCommand(
+    string FirstName,
+    string LastName,
+    DateOnly Birthdate,
+    string Street,
+    string PostalCode,
+    string City,
+    string PhoneNumber,
+    string Email,
+    string Password,
+    string Role
+    );
