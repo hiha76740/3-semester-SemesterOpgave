c7a27d64d4bcb1d35b4292011248914e00680f0c
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:23:03 +0200

    Added Register User Handler IMPL

--- BookMyHome/src/UserService/UserService.ApplicationLib/Handlers/RegisterUserHandler.cs
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Handlers/RegisterUserHandler.cs
@@ -1,5 +1,44 @@
-﻿namespace UserService.ApplicationLib.Handlers;
+﻿using UserService.ApplicationLib.Authentication;
+using UserService.ApplicationLib.Repositories;
+using UserService.DomainLib.Entities;
+using UserService.DomainLib.Enums;
+using UserService.FacadeLib.Commands.DTOs;
+using UserService.FacadeLib.Commands.Interfaces;
 
-public class RegisterUserHandler
+namespace UserService.ApplicationLib.Handlers;
+
+public class RegisterUserHandler(
+    IUserRepository userRepo,
+    IPasswordHashService passwordHashService
+    ) : IRegisterUserHandler
 {
+    async Task IRegisterUserHandler.Handle(RegisterUserCommand command)
+    {
+        var usernameExsist =  await userRepo.UsernameExsistsAsync(command.Email);
+
+        if (usernameExsist == true)
+            throw new ArgumentException("User already exsist");
+
+        if (Enum.TryParse<UserRoles>(command.Role, ignoreCase: true, out var role) == false)
+            throw new ArgumentException($"Invalid role: {command.Role}");
+
+        var hashPassword = passwordHashService.Hash(command.Password);
+
+        var user = User.Create(
+            command.FirstName,
+            command.LastName,
+            command.Birthdate,
+            command.Street,
+            command.PostalCode,
+            command.City,
+            command.PhoneNumber,
+            command.Email,
+            hashPassword,
+            role
+            );
+
+        await userRepo.RegisterUserAsync(user);
+
+        await userRepo.SaveAsync();
+    }
 }
