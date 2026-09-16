85811f61814805340e3eb10a71986b57c3bebd6f
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-16 16:22:01 +0200

    Added call to handler

--- BookMyHome/src/UserService/UserService.Api/Controllers/AuthController.cs
+++ BookMyHome/src/UserService/UserService.Api/Controllers/AuthController.cs
@@ -1,16 +1,20 @@
 ﻿using BookMyHome.ContractsLib.Requests.Users;
 using Microsoft.AspNetCore.Mvc;
+using UserService.Api.Mapper;
+using UserService.FacadeLib.Commands.Interfaces;
 
 namespace UserService.Api.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IRegisterUserHandler register) : ControllerBase
     {
         [HttpPost("register")]
-        public Task<ActionResult> Register(RegisterUserRequest request)
+        async public Task<ActionResult> Register(RegisterUserRequest request)
         {
-            throw new NotImplementedException();
+            await register.Handle(request.AsCommand());
+
+            return Ok();
         }
 
     }
