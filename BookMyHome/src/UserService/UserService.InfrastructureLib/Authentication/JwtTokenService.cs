f332d39fb0a1d5c6e2ea0f695e858c970e1b08b1
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:53:31 +0200

    Added JwtTokenService IMPL for ITokenService

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Authentication/JwtTokenService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserService.ApplicationLib.Authentication;
+using UserService.DomainLib.Entities;
+
+namespace UserService.InfrastructureLib.Authentication;
+
+public class JwtTokenService(IConfiguration configuration) : ITokenService
+{
+    string ITokenService.CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+        // 512bits 64byts = 64 char
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+            audience: configuration.GetValue<string>("AppSettings:Audience"),
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+}
