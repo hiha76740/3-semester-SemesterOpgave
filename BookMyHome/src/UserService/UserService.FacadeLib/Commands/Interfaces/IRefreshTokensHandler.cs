19baf41b25d4770cb3e99ea005ee1ee2780d057b
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 15:21:15 +0200

    added interface for refresh token handler

---
+++ BookMyHome/src/UserService/UserService.FacadeLib/Commands/Interfaces/IRefreshTokensHandler.cs
@@ -0,0 +1,8 @@
+﻿using UserService.FacadeLib.Commands.DTOs;
+
+namespace UserService.FacadeLib.Commands.Interfaces;
+
+public interface IRefreshTokensHandler
+{
+    Task<TokenDto> HandleAsync(RefreshTokenCommand command);
+}
