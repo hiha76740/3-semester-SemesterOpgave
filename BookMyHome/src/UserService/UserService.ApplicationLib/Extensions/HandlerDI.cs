bbda9207e3cf703f179159b03a5d8f52987fe1a0
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:51:13 +0200

    Added Handler DI extension method

---
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Extensions/HandlerDI.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserService.ApplicationLib.Handlers;
+using UserService.FacadeLib.Commands.Interfaces;
+
+namespace UserService.ApplicationLib.Extensions;
+
+public static class HandlerDI
+{
+    public static IServiceCollection AddHandlerDI(this IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserHandler, RegisterUserHandler>();
+        services.AddScoped<ILoginHandler, LoginHandler>();
+
+        return services;
+    }
+}
