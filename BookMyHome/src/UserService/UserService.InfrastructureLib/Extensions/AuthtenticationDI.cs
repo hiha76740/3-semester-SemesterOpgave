c59ba4e4a24872d4f08e86cbd7ea234fa977a295
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:54:03 +0200

    added AuthenticationDI extension method

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Extensions/AuthtenticationDI.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserService.ApplicationLib.Authentication;
+using UserService.InfrastructureLib.Authentication;
+
+namespace UserService.InfrastructureLib.Extensions;
+
+public static class AuthtenticationDI
+{
+    public static IServiceCollection AddAuthenTicationDI(this IServiceCollection services)
+    {
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
+        services.AddScoped<ITokenService, JwtTokenService>();
+
+        return services;
+    }
+}
