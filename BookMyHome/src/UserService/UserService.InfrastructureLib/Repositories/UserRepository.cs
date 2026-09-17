bf965902a3cdbc394107921fdd9ffbbb01b7b11a
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:55:15 +0200

    Added UserRepository IMPL

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.ApplicationLib.Repositories;
+using UserService.DomainLib.Entities;
+using UserService.InfrastructureLib.Persistence;
+
+namespace UserService.InfrastructureLib.Repositories;
+
+public class UserRepository(UserDbContext db) : IUserRepository
+{
+    Task<User?> IUserRepository.GetUserByUsernameAsync(string username)
+    {
+        return db.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+    }
+
+    async Task IUserRepository.RegisterUserAsync(User user)
+    {
+        await db.AddAsync(user);
+    }
+
+    async Task IUserRepository.SaveAsync()
+    {
+        await db.SaveChangesAsync();
+    }
+
+    Task<bool> IUserRepository.UsernameExsistsAsync(string username)
+    {
+        return db.Users.AnyAsync(u => u.Username == username);
+    }
+}
