b0bbf52df16aca5a3c54d108a2aa0f68f7e7c654
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 15:20:14 +0200

    added IMPL for refresh token handler

---
+++ BookMyHome/src/UserService/UserService.ApplicationLib/Handlers/RefreshTokenHandler.cs
@@ -0,0 +1,35 @@
+﻿using UserService.ApplicationLib.Authentication;
+using UserService.ApplicationLib.Repositories;
+using UserService.DomainLib.Entities;
+using UserService.FacadeLib.Commands.DTOs;
+using UserService.FacadeLib.Commands.Interfaces;
+
+namespace UserService.ApplicationLib.Handlers;
+
+public class RefreshTokenHandler(IUserRepository userRepo, ITokenService tokenService, IRefreshTokenService refreshTokenService) : IRefreshTokensHandler
+{
+    async Task<TokenDto?> IRefreshTokensHandler.HandleAsync(RefreshTokenCommand command)
+    {
+        var userId = new UserId(command.id);
+
+        var user = await userRepo.GetUserById(userId);
+
+        if (
+            user == null 
+            || user.RefreshToken != command.RefreshToken 
+            || user.RefreshTokenExpiryTime <= DateTime.UtcNow
+            )
+            return null;
+
+        var token = tokenService.CreateToken(user);
+        var refreshToken = refreshTokenService.GenerateRefreshToken();
+
+        user.SetRefreshToken(refreshToken);
+
+        await userRepo.SaveAsync();
+
+        var dto = new TokenDto(token, refreshToken);
+
+        return dto;
+    }
+}
