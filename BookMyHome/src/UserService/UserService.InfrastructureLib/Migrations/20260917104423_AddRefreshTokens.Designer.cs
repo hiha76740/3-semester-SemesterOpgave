a4ba07b5f1d56107acba45f3ed41b7a463f296ac
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 15:12:26 +0200

    Added new migration

--- BookMyHome/src/UserService/UserService.InfrastructureLib/Migrations/20260917104423_AddRefreshTokens.Designer.cs
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Migrations/20260917104423_AddRefreshTokens.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserService.InfrastructureLib.Persistence;
 
@@ -12,9 +13,11 @@ using UserService.InfrastructureLib.Persistence;
 namespace UserService.InfrastructureLib.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260917104423_AddRefreshTokens")]
+    partial class AddRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
