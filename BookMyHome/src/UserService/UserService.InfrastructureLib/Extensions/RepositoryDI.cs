691331bb30b85fab89a64458bbcde64fdce07803
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:54:21 +0200

    Added Repository DI Extension method

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Extensions/RepositoryDI.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserService.ApplicationLib.Repositories;
+using UserService.InfrastructureLib.Repositories;
+
+namespace UserService.InfrastructureLib.Extensions;
+
+public static class RepositoryDI
+{
+    public static IServiceCollection AddRepositoryDI(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
