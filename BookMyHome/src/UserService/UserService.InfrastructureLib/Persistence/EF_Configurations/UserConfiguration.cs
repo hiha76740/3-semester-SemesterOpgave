fd77f753bcebc354739396fad45c5be28e18e0ce
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:54:45 +0200

    Added EF User configuration

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Persistence/EF_Configurations/UserConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserService.DomainLib.Entities;
+
+namespace UserService.InfrastructureLib.Persistence.EF_Configurations;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+            .HasConversion(
+            id => id.Value,
+            value => new UserId(value)
+            );
+
+        builder.ComplexProperty(
+            u => u.Address,
+            a => a.ToJson());
+
+        builder.ComplexProperty(
+            u => u.PhoneNumber,
+            pn => pn.ToJson()
+            );
+
+        builder.ComplexProperty(
+            u => u.Email,
+            e => e.ToJson()
+            );
+    }
+}
