7dded4f886616d566e81effb4441f2618a5b52a5
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:50:37 +0200

    Added Call to DI extension methods

--- BookMyHome/src/UserService/UserService.Api/Program.cs
+++ BookMyHome/src/UserService/UserService.Api/Program.cs
@@ -1,9 +1,16 @@
 using Scalar.AspNetCore;
+using UserService.ApplicationLib.Extensions;
+using UserService.InfrastructureLib.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDatabaseDI(builder.Configuration);
+builder.Services.AddHandlerDI();
+builder.Services.AddAuthenTicationDI();
+builder.Services.AddRepositoryDI();
 
 var app = builder.Build();
 
