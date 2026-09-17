c12b549555d68cdb4f72dd48e24b9e4ff9c414bd
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 15:21:33 +0200

    added IMPL for refresh token service

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Authentication/RefreshTokenService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using UserService.ApplicationLib.Authentication;
+using UserService.DomainLib.Entities;
+
+namespace UserService.InfrastructureLib.Authentication;
+
+public class RefreshTokenService : IRefreshTokenService
+{
+    string IRefreshTokenService.GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+}
