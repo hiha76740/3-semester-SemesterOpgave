d4b587443cd9f82064aaa9b41dc0cd3b3f036798
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:54:52 +0200

    Added User Db context

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Persistence/UserDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.DomainLib.Entities;
+
+namespace UserService.InfrastructureLib.Persistence;
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserDbContext).Assembly);
+    }
+}
