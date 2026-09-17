52097d36748090ee2ffe2c3f7e68f2ab415b9ac1
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:49:31 +0200

    Added Login endpoint and completed register endpoint

--- BookMyHome/src/UserService/UserService.Api/Controllers/AuthController.cs
+++ BookMyHome/src/UserService/UserService.Api/Controllers/AuthController.cs
@@ -7,15 +7,41 @@ namespace UserService.Api.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class AuthController(IRegisterUserHandler register) : ControllerBase
+    public class AuthController(IRegisterUserHandler register, ILoginHandler login) : ControllerBase
     {
         [HttpPost("register")]
         async public Task<ActionResult> Register(RegisterUserRequest request)
         {
-            await register.Handle(request.AsCommand());
+            try
+            {
+                await register.Handle(request.AsRegisterCommand());
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginRequest request)
+        {
+            try
+            {
+                var token = await login.HandleAsync(request.AsLoginCommand());
+
+                if (token == null)
+                    return BadRequest("Invalid username or password");
+
+                return Ok(token);
+            }
+            catch (Exception ex) 
+            {
+
+                return BadRequest(ex);
+            }
+        }
     }
 }
