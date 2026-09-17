73a018e2e13eede15e695c9eee93674b7312fbe6
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-17 08:54:11 +0200

    Added DatabaseDI extension method

---
+++ BookMyHome/src/UserService/UserService.InfrastructureLib/Extensions/DatabaseDI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserService.InfrastructureLib.Persistence;
+
+namespace UserService.InfrastructureLib.Extensions;
+
+public static class DatabaseDI
+{
+    public static IServiceCollection AddDatabaseDI(this IServiceCollection services, IConfiguration configuration)
+    {
+        var conn = configuration.GetConnectionString("UserDB");
+        services.AddDbContext<UserDbContext>(options =>
+        options.UseSqlServer(conn));
+
+        return services;
+    }
+}
