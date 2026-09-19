6e614b7dff73e554eb9d6aabc0447de971d6fd91
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-19 06:16:44 +0200

    updated so only price comes from body other parameters is in the path

--- BookMyHome/src/Shared/BookMyHome.ContractsLib/Requests/Accomodations/UpdateListingDailyPriceRequest.cs
+++ BookMyHome/src/Shared/BookMyHome.ContractsLib/Requests/Accomodations/UpdateListingDailyPriceRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace BookMyHome.ContractsLib.Requests.Accomodations;
 
-public record UpdateListingDailyPriceRequest(Guid AccomodationId, Guid ListingId, decimal Price);
+public record UpdateListingDailyPriceRequest(decimal Price);
