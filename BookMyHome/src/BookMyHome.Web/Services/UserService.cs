aca258fb00b43c6584d27b78d65614fcf9195932
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-20 07:42:55 +0200

    Added IMPL for User Service

---
+++ BookMyHome/src/BookMyHome.Web/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using BookMyHome.Web.Models;
+using System.Net.Http.Json;
+
+namespace BookMyHome.Web.Services
+{
+    public class UserService(HttpClient httpClient) : IUserService
+    {
+        private readonly string baseUrl = "https://localhost:7157/";
+
+        Task IUserService.Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<int> IUserService.Register(UserModel model)
+        {
+            var response = await httpClient.PostAsJsonAsync($"{baseUrl}api/v1/Auth/register", model);
+            var responseStatusCode = response.StatusCode;
+            return (int)responseStatusCode;
+        }
+    }
+}
