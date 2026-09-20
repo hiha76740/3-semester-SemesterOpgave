84bad1d9d8e689337d0df3ab48afe02347c61da0
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-20 07:42:02 +0200

    added UserModel

---
+++ BookMyHome/src/BookMyHome.Web/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMyHome.Web.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string FirstName { get; set; } = "Harry";
+        [Required]
+        public string LastName { get; set; } = "Potter";
+        [Required]
+        public DateOnly Birthdate { get; set; } = new DateOnly(1987,07,30);
+        [Required]
+        public string Street { get; set; } = "Legustervænget 7";
+        [Required]
+        public string PostalCode { get; set; } = "7100";
+        [Required]
+        public string City { get; set; } = "Vejle";
+        [Required]
+        public string PhoneNumber { get; set; } = "95175382";
+        [Required]
+        public string Email { get; set; } = "HarryPotter@Hogwarts.test";
+        [Required]
+        public string Password { get; set; } = "Hedwig";
+        [Required]
+        public string Role { get; set; } = "Guest";
+    }
+}
