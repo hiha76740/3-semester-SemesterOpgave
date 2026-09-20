adc232b64ea3b6ff6d20afb69ae8fc1e719f8d11
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-20 11:37:40 +0200

    added interface and IMPL for cookie service

---
+++ BookMyHome/src/UserService/UserService.Api/Services/ICookieService.cs
@@ -0,0 +1,9 @@
+﻿using UserService.FacadeLib.Commands.DTOs;
+
+namespace UserService.Api.Services
+{
+    public interface ICookieService
+    {
+        void SetTokenInsideCookie(TokenDto token, HttpContext context);
+    }
+}
