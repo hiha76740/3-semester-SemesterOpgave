ca015b673e2f9b2e69bcaf5c46d233344a55a9fe
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-20 07:39:32 +0200

    Added Radzen

--- BookMyHome/src/BookMyHome.Web/Program.cs
+++ BookMyHome/src/BookMyHome.Web/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BookMyHome.Web;
+using Radzen;
+using BookMyHome.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRadzenComponents();
+
+builder.Services.AddScoped<IUserService, UserService>();
 
 await builder.Build().RunAsync();
