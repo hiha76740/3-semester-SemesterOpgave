b429621aa3fe66d11c9808844380e253a21218a6
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-21 08:27:10 +0200

    Added new AuthService DI

--- BookMyHome/src/BookMyHome.Web/Program.cs
+++ BookMyHome/src/BookMyHome.Web/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
 
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserService, UserService>();
 
 await builder.Build().RunAsync();
