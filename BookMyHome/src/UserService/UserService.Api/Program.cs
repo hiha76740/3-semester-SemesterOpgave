5375489c0b7421da593e448a92b5f0c629dccf9b
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-21 18:20:37 +0200

    Revert "added allowCredentials"
    This reverts commit 5c04c30b29de330dccc250c6ed10fb87303feb80.

--- BookMyHome/src/UserService/UserService.Api/Program.cs
+++ BookMyHome/src/UserService/UserService.Api/Program.cs
@@ -54,11 +54,13 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowBlazorOrigin", policy =>
     {
-        // Allow the exact client origins and allow credentials so cookies are accepted by the browser
-        policy.WithOrigins("https://localhost:7179", "https://localhost:8082")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+        policy.WithOrigins("https://localhost:7179")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+
+        policy.WithOrigins("https://localhost:8082")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
     });
 });
 
