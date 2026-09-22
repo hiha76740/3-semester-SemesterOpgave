e09dad2d3f2b98255d50e0d549e390b08559e56f
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-22 07:17:44 +0200

    added both origins to 1 and added AllowCredentials

--- BookMyHome/src/UserService/UserService.Api/Program.cs
+++ BookMyHome/src/UserService/UserService.Api/Program.cs
@@ -54,13 +54,12 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowBlazorOrigin", policy =>
     {
-        policy.WithOrigins("https://localhost:7179")
+        policy.WithOrigins(
+            "https://localhost:7179",
+            "https://localhost:8082")
         .AllowAnyHeader()
-        .AllowAnyMethod();
-
-        policy.WithOrigins("https://localhost:8082")
-        .AllowAnyHeader()
-        .AllowAnyMethod();
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
