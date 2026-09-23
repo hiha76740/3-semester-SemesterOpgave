a1f4d807db0b6d8b26c792760a5a8aa44583e636
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-23 08:22:55 +0200

    added interface for listing service

---
+++ BookMyHome/src/BookMyHome.Web/Service Interfaces/IListingService.cs
@@ -0,0 +1,9 @@
+﻿using BookMyHome.ContractsLib.Responses.Accomodations;
+
+namespace BookMyHome.Web.Service_Interfaces
+{
+    public interface IListingService
+    {
+        Task<IReadOnlyList<ListingResponse>> GetAllListings();
+    }
+}
