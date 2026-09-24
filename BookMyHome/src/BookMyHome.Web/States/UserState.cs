0e74eda9d94adc40d5b227d36c921fef96a9eede
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-24 07:36:52 +0200

    added user state

---
+++ BookMyHome/src/BookMyHome.Web/States/UserState.cs
@@ -0,0 +1,11 @@
+﻿namespace BookMyHome.Web.States
+{
+    public class UserState
+    {
+        public Guid? UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Role { get; set; }
+    }
+}
