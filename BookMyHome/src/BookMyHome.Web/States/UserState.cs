f58c4e50c3447d49bb10cb6cd228ba4271979e6e
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-25 05:39:52 +0200

    updated UserState to include event and set method

--- BookMyHome/src/BookMyHome.Web/States/UserState.cs
+++ BookMyHome/src/BookMyHome.Web/States/UserState.cs
@@ -2,10 +2,23 @@
 {
     public class UserState
     {
-        public Guid? UserId { get; set; }
-        public string? Username { get; set; }
-        public string? Firstname { get; set; }
-        public string? Lastname { get; set; }
-        public string? Role { get; set; }
+        public event Action? OnChange;
+
+        public Guid? UserId { get; private set; }
+        public string? Username { get; private set; }
+        public string? Firstname { get; private set; }
+        public string? Lastname { get; private set; }
+        public string? Role { get; private set; }
+
+        public void SetUser(Guid id, string firstname, string lastname,
+            string username, string role)
+        {
+            UserId = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Username = username;
+            Role = role;
+            OnChange?.Invoke();
+        }
     }
 }
