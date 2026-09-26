cca4b117f812ba1cc6373d25e0930ca8723546ca
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-26 07:11:16 +0200

    Add transactional booking creation & overlap check
    Introduce IUnitOfWork and transaction handling in the booking handler. Add required usings, wrap handler logic in try/catch, and begin a serializable transaction (uow.BeginTransaction(IsolationLevel.Serializable)) with Commit on success and Rollback on exception. Remove loading all bookings; use bookingRepo.HasOverlapingBookingAsync(...) to detect overlaps and throw OverlapFoundException when detected. Persist booking via CreateAsync and SaveAsync inside the transaction. Wrap unexpected errors as ApplicationException with the original exception as inner exception.

--- BookMyHome/src/BookingService/BookingService.ApplicationLib/Handlers/CreateBookingHandler.cs
+++ BookMyHome/src/BookingService/BookingService.ApplicationLib/Handlers/CreateBookingHandler.cs
@@ -1,40 +1,72 @@
-﻿using BookingService.ApplicationLib.Repositories;
+﻿using BookingService.ApplicationLib.Exceptions;
+using BookingService.ApplicationLib.Repositories;
 using BookingService.ApplicationLib.Services;
+using BookingService.ApplicationLib.UnitOfWork;
 using BookingService.DomainLib.Entities;
 using BookingService.DomainLib.ValueObjects;
 using BookingService.FacadeLib.Commands.DTOs;
 using BookingService.FacadeLib.Commands.Interfaces;
 using Shared.BookMyHome.SharedKernelLib.Exceptions;
+using System.Data;
 
 namespace BookingService.ApplicationLib.Handlers;
 
-public class CreateBookingHandler(IGuestService guestService, IAccomodationService accomodationService, IBookingRepository bookingRepo) : ICreateBookingHandler
+public class CreateBookingHandler(
+    IGuestService guestService,
+    IAccomodationService accomodationService,
+    IBookingRepository bookingRepo,
+    IUnitOfWork uow) : ICreateBookingHandler
 {
     async Task ICreateBookingHandler.Handle(CreateBookingCommand command)
     {
-        var guestId = new GuestId(command.GuestId);
-        var accomodationId = new AccomodationId(command.AccomodationId);
+        try
+        {
+            var guestId = new GuestId(command.GuestId);
+            var accomodationId = new AccomodationId(command.AccomodationId);
 
-        var guestExistTask = guestService.GuestExistAsync(guestId);
-        var accomodationExistTask = accomodationService.AccomodationExistAsync(accomodationId);
-        var existingBookingsTask = bookingRepo.GetAllAsync();
+            var guestExistTask = guestService.GuestExistAsync(guestId);
+            var accomodationExistTask = accomodationService.AccomodationExistAsync(accomodationId);
 
-        await Task.WhenAll(guestExistTask, accomodationExistTask, existingBookingsTask);
+            await Task.WhenAll(guestExistTask, accomodationExistTask);
 
-        var guestExist = await guestExistTask;
-        var accomodationExist = await accomodationExistTask;
-        var existingBookings = await existingBookingsTask;
+            var guestExist = await guestExistTask;
+            var accomodationExist = await accomodationExistTask;
 
-        if (guestExist == false)
-            throw new NotFoundException("Guest not found doing booking creation");
+            if (guestExist == false)
+                throw new NotFoundException("Guest not found doing booking creation");
 
-        if (accomodationExist == false)
-            throw new NotFoundException("Accomodation not found doing booking creation");
+            if (accomodationExist == false)
+                throw new NotFoundException("Accomodation not found doing booking creation");
 
-        var booking = Booking.Create(guestId, accomodationId, command.StartDate, command.EndDate, command.Price, existingBookings);
 
-        await bookingRepo.CreateAsync(booking);
+            uow.BeginTransaction(
+                IsolationLevel.Serializable);
 
-        await bookingRepo.SaveAsync();
+            var overlapExsists = await bookingRepo.HasOverlapingBookingAsync(
+                accomodationId,
+                command.StartDate,
+                command.EndDate);
+
+            if (overlapExsists == true)
+                throw new OverlapFoundException("Booking overlap found, booking aborted");
+
+            var booking = Booking.Create(
+                guestId, 
+                accomodationId,
+                command.StartDate,
+                command.EndDate, 
+                command.Price
+                );
+
+            await bookingRepo.CreateAsync(booking);
+
+            await bookingRepo.SaveAsync();
+            uow.Commit();
+        }
+        catch (Exception ex)
+        {
+            uow.Rollback();
+            throw new ApplicationException("Error while creating booking",ex);
+        }
     }
 }
