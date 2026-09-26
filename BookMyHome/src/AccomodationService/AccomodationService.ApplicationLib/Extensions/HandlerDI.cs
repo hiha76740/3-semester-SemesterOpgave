d890689a7f221a00cd507115e1b78ad99396d10b
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-26 19:27:47 +0200

    updated HandlerDI with new facility handlers

--- BookMyHome/src/AccomodationService/AccomodationService.ApplicationLib/Extensions/HandlerDI.cs
+++ BookMyHome/src/AccomodationService/AccomodationService.ApplicationLib/Extensions/HandlerDI.cs
@@ -1,5 +1,9 @@
-﻿using AccomodationService.ApplicationLib.Handlers;
-using AccomodationService.FacadeLib.Commands.Interfaces;
+﻿using AccomodationService.ApplicationLib.Handlers.Accomodations;
+using AccomodationService.ApplicationLib.Handlers.Facilities;
+using AccomodationService.ApplicationLib.Handlers.Listings;
+using AccomodationService.FacadeLib.Commands.Interfaces.Accomodations;
+using AccomodationService.FacadeLib.Commands.Interfaces.Facilities;
+using AccomodationService.FacadeLib.Commands.Interfaces.Listings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AccomodationService.ApplicationLib.Extensions;
@@ -13,6 +17,8 @@ public static class HandlerDI
         services.AddScoped<IUpdateListingDailyPriceHandler, UpdateListingDailyPriceHandler>();
         services.AddScoped<IUpdateListingHouseRulesHandler, UpdateListingHouseRulesHandler>();
         services.AddScoped<IDeleteListingHandler, DeleteListingHandler>();
+        services.AddScoped<IAddFacilityHandler, AddFacilityHandler>();
+        services.AddScoped<IRemoveFacilityHandler, RemoveFacilityHandler>();
 
         return services;
     }
