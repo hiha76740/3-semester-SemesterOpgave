57a6e0a55af7b0527cd4bac34f64e61fd9fbc6f4
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-26 07:12:46 +0200

    added unit of work impl

---
+++ BookMyHome/src/BookingService/BookingService.InfrastructureLib/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using BookingService.ApplicationLib.UnitOfWork;
+using BookingService.InfrastructureLib.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+
+namespace BookingService.InfrastructureLib.UnitOfWork
+{
+    public class UnitOfWork(BookingDbContext db) : IUnitOfWork
+    {
+        private IDbContextTransaction _transaction = null!;
+
+        void IUnitOfWork.BeginTransaction(IsolationLevel isolationLevel)
+        {
+            if (db.Database.CurrentTransaction != null)
+                throw new InvalidOperationException("Transaction could not start");
+
+            _transaction = db.Database.BeginTransaction(isolationLevel);
+
+        }
+
+        void IUnitOfWork.Commit()
+        {
+            _transaction.Commit();
+            _transaction.Dispose();
+        }
+
+        void IUnitOfWork.Rollback()
+        {
+            _transaction.Rollback();
+            _transaction.Dispose();
+        }
+    }
+}
