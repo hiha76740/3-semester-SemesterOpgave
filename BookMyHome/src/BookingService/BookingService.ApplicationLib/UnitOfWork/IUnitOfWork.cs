b292c4f36c0300389c68df29f520976cf566c4ee
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-26 07:11:35 +0200

    Added interface for unit of work

---
+++ BookMyHome/src/BookingService/BookingService.ApplicationLib/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace BookingService.ApplicationLib.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    void Commit();
+
+    void Rollback();
+
+    void BeginTransaction(IsolationLevel isolationLevel);
+}
