59445b5d99bfd7663bbc6bd03182d2fa75dc69c9
Henrik Ivang Hansen <Ivang_6464@hotmail.com>
2026-09-26 07:58:37 +0200

    Added new custom exception

---
+++ BookMyHome/src/Shared/BookMyHome.SharedKernelLib/Exceptions/ConcurrencyConflictException.cs
@@ -0,0 +1,6 @@
+﻿namespace BookMyHome.SharedKernelLib.Exceptions;
+
+public class ConcurrencyConflictException : Exception
+{
+    public ConcurrencyConflictException(string message, Exception innerException) : base(message, innerException) { }
+}
